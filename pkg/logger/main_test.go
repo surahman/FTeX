3e469a411a2b01401a644fee65c070ccf411f278
Saad Ur Rahman <saadurrahman@apache.org>
2022-12-14 22:13:20 -0500

    [Tests] Zap logger config loading and logging method testing.

--- pkg/logger/main_test.go
+++ pkg/logger/main_test.go
@@ -1,4 +1,65 @@
 package logger
 
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
 // loggerConfigTestData is a map of Zap logger configuration test data.
 var loggerConfigTestData = configTestData()
+
+// testLogSpy is a testing.TB that captures logged messages.
+type testLogSpy struct {
+	testing.TB
+	failed   bool
+	Messages []string
+}
+
+func newTestLogSpy(t testing.TB) *testLogSpy {
+	return &testLogSpy{TB: t}
+}
+
+func (t *testLogSpy) Fail() {
+	t.failed = true
+}
+
+func (t *testLogSpy) Failed() bool {
+	return t.failed
+}
+
+func (t *testLogSpy) FailNow() {
+	t.Fail()
+	t.TB.FailNow()
+}
+
+func (t *testLogSpy) Logf(format string, args ...interface{}) {
+	// Log messages are in the format,
+	//
+	//   2017-10-27T13:03:01.000-0700	DEBUG	your message here	{data here}
+	//
+	// We strip the first part of these messages because we can't really test
+	// for the timestamp from these tests.
+	m := fmt.Sprintf(format, args...)
+	m = m[strings.IndexByte(m, '\t')+1:]
+	t.Messages = append(t.Messages, m)
+	t.TB.Log(m)
+}
+
+func (t *testLogSpy) AssertMessages(msgs ...string) {
+	require.Equal(t.TB, msgs, t.Messages, "logged messages did not match")
+}
+
+func (t *testLogSpy) AssertPassed() {
+	t.assertFailed(false, "expected test to pass")
+}
+
+func (t *testLogSpy) AssertFailed() {
+	t.assertFailed(true, "expected test to fail")
+}
+
+func (t *testLogSpy) assertFailed(v bool, msg string) {
+	require.Equal(t.TB, v, t.failed, msg)
+}
