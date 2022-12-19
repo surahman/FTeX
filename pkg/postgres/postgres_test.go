748627f91bd83c9eb523e04f0c852a90a5516d7c
Saad Ur Rahman <saadurrahman@apache.org>
2022-12-18 19:59:57 -0500

    [Tests] new postgres implementation object.

--- pkg/postgres/postgres_test.go
+++ pkg/postgres/postgres_test.go
@@ -59,3 +59,41 @@ func TestNewPostgres(t *testing.T) {
 		})
 	}
 }
+
+func TestNewPostgresImpl(t *testing.T) {
+	testCases := []struct {
+		name      string
+		fileName  string
+		input     string
+		expectErr require.ErrorAssertionFunc
+		expectNil require.ValueAssertionFunc
+	}{
+		// ----- test cases start ----- //
+		{
+			name:      "File found",
+			fileName:  constants.GetPostgresFileName(),
+			input:     postgresConfigTestData["test_suite"],
+			expectErr: require.NoError,
+			expectNil: require.NotNil,
+		}, {
+			name:      "File not found",
+			fileName:  "wrong_file_name.yaml",
+			input:     postgresConfigTestData["test_suite"],
+			expectErr: require.Error,
+			expectNil: require.Nil,
+		},
+		// ----- test cases end ----- //
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			// Configure mock filesystem.
+			fs := afero.NewMemMapFs()
+			require.NoError(t, fs.MkdirAll(constants.GetEtcDir(), 0644), "Failed to create in memory directory")
+			require.NoError(t, afero.WriteFile(fs, constants.GetEtcDir()+testCase.fileName, []byte(testCase.input), 0644), "Failed to write in memory file")
+
+			c, err := newPostgresImpl(&fs, zapLogger)
+			testCase.expectErr(t, err)
+			testCase.expectNil(t, c)
+		})
+	}
+}
