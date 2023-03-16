04e44708d6a7e04445a30edbf5ee567fe7c3b3fb
Saad Ur Rahman <saadurrahman@apache.org>
2023-03-16 16:58:30 -0400

    [Postgres] fiat currency accounts.

--- SQL/schema_migration.sql
+++ SQL/schema_migration.sql
@@ -26,5 +26,54 @@ CREATE TYPE currency AS ENUM (
 'MVR','MWK','MXN','MYR','MZN','NAD','NGN','NIO','NOK','NPR','NZD','OMR','PAB','PEN','PGK','PHP','PKR','PLN','PYG',
 'QAR','RON','RSD','RUB','RWF','SAR','SBD','SCR','SDG','SEK','SGD','SHP','SLL','SOS','SPL','SRD','STN','SVC','SYP',
 'SZL','THB','TJS','TMT','TND','TOP','TRY','TTD','TVD','TWD','TZS','UAH','UGX','USD','UYU','UZS','VEF','VND','VUV',
-'WST','XAF','XCD','XDR','XOF','XPF','YER','ZAR','ZMW','ZWD');
+'WST','XAF','XCD','XDR','XOF','XPF','YER','ZAR','ZMW','ZWD',
+'DEPOSIT');
 --rollback DROP TYPE currency;
+
+--changeset surahman:3
+--preconditions onFail:HALT onError:HALT
+--comment: Fiat currency accounts.
+CREATE TABLE IF NOT EXISTS fiat_accounts (
+    currency        CURRENCY        DEFAULT 'USD' NOT NULL,
+    balance         NUMERIC(2)      DEFAULT 0 NOT NULL,
+    last_tx         NUMERIC(2)      DEFAULT 0 NOT NULL,
+    last_tx_ts      TIMESTAMPTZ     DEFAULT now() NOT NULL,
+    created_at      TIMESTAMPTZ     DEFAULT now() NOT NULL,
+    client_id       UUID            REFERENCES users(client_id) ON DELETE CASCADE,
+    account_number  UUID            PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS fiat_client_id_idx ON fiat_accounts USING btree (client_id);
+--rollback DROP TABLE fiat_accounts CASCADE;
+
+--changeset surahman:4
+--preconditions onFail:HALT onError:HALT
+--comment: Create fiat currency deposit user and account.
+INSERT INTO users (
+    first_name,
+    last_name,
+    email,
+    username,
+    password,
+    is_deleted)
+SELECT
+   'Internal',
+   'FTeX, Inc.',
+   'deposit@ftex.com',
+   'deposit-fiat',
+   password,
+   true
+FROM
+    substr(md5(random()::text), 0, 32) AS password;
+
+INSERT INTO fiat_accounts (
+    currency,
+    client_id)
+SELECT
+   'DEPOSIT',
+   client_id
+FROM
+    users AS client_id
+WHERE
+    username = 'deposit-fiat';
+--rollback DELETE FROM users WHERE username='deposit-fiat';
--- SQL/tablespaces.sql
+++ SQL/tablespaces.sql
@@ -1,1 +1,2 @@
 CREATE TABLESPACE users_data LOCATION '/table_data/ftex_users';
+CREATE TABLESPACE fiat_accounts_data LOCATION '/table_data/ftex_fiat_accounts';
