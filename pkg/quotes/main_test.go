a1b133e383ef55f77c60a7aa6ac490b783d4809d
Saad Ur Rahman <saadurrahman@apache.org>
2023-04-11 22:40:39 -0400

    [Tests] loading API configurations based on run environment.

--- pkg/quotes/main_test.go
+++ pkg/quotes/main_test.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/spf13/afero"
 	"github.com/surahman/FTeX/pkg/constants"
 	"github.com/surahman/FTeX/pkg/logger"
 	"go.uber.org/zap"
@@ -16,7 +17,7 @@ import (
 var quotesConfigTestData = configTestData()
 
 // testConfigs is the name of the Quotes configuration to use in the tests.
-var testConfigs string
+var testConfigs *config
 
 // zapLogger is the Zap logger used strictly for the test suite in this package.
 var zapLogger *logger.Logger
@@ -58,19 +59,59 @@ func setup() error {
 		return nil
 	}
 
-	// If running on a GitHub Actions runner use the secret stored in the GitHub Actions Secrets.
-	testConfigs = "test_suite"
+	var (
+		rawConfigs string
+		fs         afero.Fs
+	)
 
+	// If running on a GitHub Actions runner use the secret stored in the GitHub Actions Secrets.
 	if _, ok := os.LookupEnv(constants.GetGithubCIKey()); ok {
 		zapLogger.Info("Integration Test running on Github CI runner.")
 		zapLogger.Warn("*** Please ensure that the Quotes configurations are upto date in GHA Secrets ***")
 
-		if testConfigs, ok = os.LookupEnv(constants.GetQuotesPrefix() + "_CI_CONFIGS"); !ok {
+		if rawConfigs, ok = os.LookupEnv(constants.GetQuotesPrefix() + "_CI_CONFIGS"); !ok {
 			msg := "failed to load Quotes configs from GitHub Actions Secrets"
 			zapLogger.Error(msg)
 
 			return fmt.Errorf(msg)
 		}
+	} else {
+		fsDevLoader := afero.Afero{
+			Fs: afero.NewOsFs(),
+		}
+
+		var bytes []byte
+
+		bytes, err := fsDevLoader.ReadFile("../../configs/DevQuotesConfig.yaml")
+		if err != nil || len(bytes) < 1 {
+			zapLogger.Error("Please ensure there are API Keys and Endpoints configured in" +
+				"\".configs/DevQuotesConfig.yaml\" for development.")
+
+			return fmt.Errorf("failed to read dev confiq file, number of bytes: %d %w", len(bytes), err)
+		}
+
+		rawConfigs = string(bytes)
+	}
+
+	// Configure in memory file system to load configs.
+	fs = afero.NewMemMapFs()
+
+	if err := fs.MkdirAll(constants.GetEtcDir(), 0644); err != nil {
+		return fmt.Errorf("failed to create in memory directory %w", err)
+	}
+
+	if err := afero.WriteFile(
+		fs,
+		constants.GetEtcDir()+constants.GetQuotesFileName(),
+		[]byte(rawConfigs),
+		0644); err != nil {
+		return fmt.Errorf("failed to write in memory file %w", err)
+	}
+
+	// Load configs for tests.
+	testConfigs = newConfig()
+	if err := testConfigs.Load(fs); err != nil {
+		return fmt.Errorf("failed to load test configs %w", err)
 	}
 
 	return nil
