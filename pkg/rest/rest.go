1895cce3ee7d6123c0e1eab2e4758d2533b5a569
Saad Ur Rahman <saadurrahman@apache.org>
2023-04-15 11:36:18 -0400

    [REST] base handler setup method.

--- pkg/rest/rest.go
+++ pkg/rest/rest.go
@@ -9,6 +9,8 @@ import (
 	"github.com/surahman/FTeX/pkg/logger"
 	"github.com/surahman/FTeX/pkg/postgres"
 	"github.com/surahman/FTeX/pkg/redis"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 // Format and generate Swagger UI files using makefile.
@@ -43,4 +45,35 @@ func NewServer(fs *afero.Fs, auth auth.Auth, postgres postgres.Postgres, redis r
 			wg:     wg,
 		},
 		err
+}
+
+// initialize will configure the HTTP server routes.
+func (s *Server) initialize() {
+	s.router = gin.Default()
+
+	//	@title						FTeX, Incorporated. (Formerly Crypto-Bro's Bank, Inc.)
+	//	@version					1.0.0
+	//	@description				FTeX Fiat and Cryptocurrency Banking API.
+	//	@description				Bank, buy, and sell Fiat and Cryptocurrencies. Prices for all currencies are
+	//	@description				retrieved from real-time quote providers.
+	//
+	//	@schemes					http
+	//	@host						localhost:33723
+	//	@BasePath					/api/rest/v1
+	//
+	//	@accept						json
+	//	@produce					json
+	//
+	//	@contact.name				Saad Ur Rahman
+	//	@contact.url				https://www.linkedin.com/in/saad-ur-rahman/
+	//	@contact.email				saad.ur.rahman@gmail.com
+	//
+	//	@license.name				GPL-3.0
+	//	@license.url				https://opensource.org/licenses/GPL-3.0
+	//
+	//	@securityDefinitions.apikey	ApiKeyAuth
+	//	@in							header
+	//	@name						Authorization
+
+	s.router.GET(s.conf.Server.SwaggerPath, ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
