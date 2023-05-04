f66cdb5b081974edcb1e947454635bedb560f5c1
Saad Ur Rahman <saadurrahman@apache.org>
2023-05-03 22:02:16 -0400

    [Utilities] HTTP Fiat balance request (paginated) query string unpack.

---
+++ pkg/utilities/http.go
@@ -0,0 +1,44 @@
+package utilities
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/surahman/FTeX/pkg/auth"
+	"github.com/surahman/FTeX/pkg/postgres"
+)
+
+// HTTPFiatBalancePaginatedRequest will convert the encrypted URL query parameter for the currency and the record limit
+// and covert them to a currency and integer record limit.
+func HTTPFiatBalancePaginatedRequest(auth auth.Auth, currencyStr, limitStr string) (postgres.Currency, int32, error) {
+	var (
+		currency  postgres.Currency
+		decrypted []byte
+		err       error
+		limit     int64
+	)
+
+	// Decrypt currency string and covert to currency struct.
+	if decrypted, err = auth.DecryptFromString(currencyStr); err != nil {
+		return currency, -1, fmt.Errorf("failed to decrypt next currency")
+	}
+
+	if len(decrypted) == 0 {
+		decrypted = []byte("AED")
+	}
+
+	if err = currency.Scan(string(decrypted)); err != nil {
+		return currency, -1, fmt.Errorf("failed to parse currency")
+	}
+
+	// Convert record limit to int and set base bound for bad input.
+	if limit, err = strconv.ParseInt(limitStr, 10, 32); err != nil {
+		return currency, -1, fmt.Errorf("failed to parse record limit")
+	}
+
+	if limit < 1 {
+		limit = 10
+	}
+
+	return currency, int32(limit), nil
+}
---
+++ pkg/utilities/http_test.go
@@ -0,0 +1,82 @@
+package utilities
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/surahman/FTeX/pkg/postgres"
+)
+
+func TestUtilities_HTTPFiatBalancePaginatedRequest(t *testing.T) {
+	t.Parallel()
+
+	encEmpty, err := testAuth.EncryptToString([]byte(""))
+	require.NoError(t, err, "failed to encrypt empty currency.")
+
+	encAED, err := testAuth.EncryptToString([]byte("AED"))
+	require.NoError(t, err, "failed to encrypt AED currency.")
+
+	encUSD, err := testAuth.EncryptToString([]byte("USD"))
+	require.NoError(t, err, "failed to encrypt USD currency.")
+
+	encEUR, err := testAuth.EncryptToString([]byte("EUR"))
+	require.NoError(t, err, "failed to encrypt EUR currency.")
+
+	testCases := []struct {
+		name           string
+		currencyStr    string
+		limitStr       string
+		expectCurrency postgres.Currency
+		expectLimit    int32
+	}{
+		{
+			name:           "empty currency",
+			currencyStr:    encEmpty,
+			limitStr:       "5",
+			expectCurrency: postgres.CurrencyAED,
+			expectLimit:    5,
+		}, {
+			name:           "AED",
+			currencyStr:    encAED,
+			limitStr:       "5",
+			expectCurrency: postgres.CurrencyAED,
+			expectLimit:    5,
+		}, {
+			name:           "USD",
+			currencyStr:    encUSD,
+			limitStr:       "5",
+			expectCurrency: postgres.CurrencyUSD,
+			expectLimit:    5,
+		}, {
+			name:           "EUR",
+			currencyStr:    encEUR,
+			limitStr:       "5",
+			expectCurrency: postgres.CurrencyEUR,
+			expectLimit:    5,
+		}, {
+			name:           "base bound limit",
+			currencyStr:    encEUR,
+			limitStr:       "0",
+			expectCurrency: postgres.CurrencyEUR,
+			expectLimit:    10,
+		}, {
+			name:           "above base bound limit",
+			currencyStr:    encEUR,
+			limitStr:       "999",
+			expectCurrency: postgres.CurrencyEUR,
+			expectLimit:    999,
+		},
+	}
+
+	for _, testCase := range testCases {
+		test := testCase
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			actualCurr, actualLimit, err := HTTPFiatBalancePaginatedRequest(testAuth, test.currencyStr, test.limitStr)
+			require.NoError(t, err, "error returned from query unpacking")
+			require.Equal(t, test.expectCurrency, actualCurr, "currencies mismatched.")
+			require.Equal(t, test.expectLimit, actualLimit, "request limit size mismatched.")
+		})
+	}
+}
