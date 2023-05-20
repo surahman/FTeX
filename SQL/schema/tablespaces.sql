77cef7237a0e8bcc454fbd9c1c4346d85cf1b71c
Saad Ur Rahman <saadurrahman@apache.org>
2023-05-19 20:49:12 -0400

    [SQL] tablespace migration scripts.

--- SQL/schema/tablespaces.sql
+++ SQL/schema/tablespaces.sql
@@ -1,3 +1,5 @@
 CREATE TABLESPACE users_data LOCATION '/table_data/ftex_users';
 CREATE TABLESPACE fiat_accounts_data LOCATION '/table_data/ftex_fiat_accounts';
-CREATE TABLESPACE fiat_general_ledger_data LOCATION '/table_data/ftex_fiat_general_ledger';
+CREATE TABLESPACE fiat_journal_data LOCATION '/table_data/ftex_fiat_journal';
+CREATE TABLESPACE crypto_accounts_data LOCATION '/table_data/ftex_crypto_accounts';
+CREATE TABLESPACE crypto_journal_data LOCATION '/table_data/ftex_crypto_journal';
