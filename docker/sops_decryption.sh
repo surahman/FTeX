41477cd4bd65b9cc251130b84a92d034f0cbcd79
Saad Ur Rahman <saadurrahman@apache.org>
2023-07-23 13:09:34 -0400

    [Docker] decryption bash script.

--- docker/sops_decryption.sh
+++ docker/sops_decryption.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+configFilePath=$1
+encryptExt=sops
+decryptExt=yaml
+
+###############################################################################
+#               Decrypt Secret OperationS Configuration Files                 #
+###############################################################################
+
+echo 'Decrypting Secret OperationS configuration files...'
+
+for item in "$configFilePath"/*."$encryptExt"; do
+  filename=$(basename "$item" ."$encryptExt")
+
+  sops -d --input-type yaml --output-type yaml "$item" > "$configFilePath"/"$filename"."$decryptExt" ||
+  { echo "Decrypting $item failed"; exit 1; }
+
+  rm "$item"
+done
+
+echo 'Decryption completed successfully.'
