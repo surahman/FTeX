72c3010e013a2876820549b7f06a81b4f9d2a617
Saad Ur Rahman <saadurrahman@apache.org>
2025-11-27 16:20:59 -0500

    [Quotes] api no longer returns error on failure just status code.

--- pkg/quotes/quotes_test.go
+++ pkg/quotes/quotes_test.go
@@ -114,7 +114,6 @@ func TestQuotesImpl_FiatQuote(t *testing.T) {
 
 			if err != nil {
 				require.NotEqual(t, 200, result.Error.Code, "received valid response code on error.")
-				require.NotEmpty(t, result.Error.Type, "received no type on error.")
 
 				return
 			}
